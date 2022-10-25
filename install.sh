2d3f045341ef730ddddcdc4f9af3a7c6c46b7c59
Waveyyyy <waves-sec@protonmail.com>
2022-10-26 00:44:59 +0100

    Refined install.sh to work with forthcoming changes

--- install.sh
+++ install.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-stow -t ~/ -d dots $(ls dots)
+#set -x
+
+options=($(ls | rg -v "\."))
+
+function list_options() {
+	printf "Available options:\n"
+	for i in "${options[@]}"; do
+		printf "\t- %s\n" $i
+	done
+}
+
+if [[ -z $1 ]]; then
+	printf "./install.sh [folder]\n"
+	list_options
+	exit 1
+elif [[ ! " ${options[*]} " =~ " ${1} " ]]; then
+	printf "%s is not a valid option\n" $1
+	list_options
+	exit 2
+else 
+	stow -t ~/ -d "$1" $(ls $1)
+fi
+
