d9f0f949750b9bdda4a45a45772c5a787ef3a29c
Waveyyyy <waves-sec@protonmail.com>
2022-10-28 21:15:04 +0100

    Renamed onedark.lua to theme.lua and changed theme to gruvbox

---
+++ desktop/neovim/.config/nvim/after/plugin/theme.lua
@@ -0,0 +1,25 @@
+require("gruvbox").setup({
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = true,
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+})
+vim.o.background = "dark"
+vim.cmd("colorscheme gruvbox")
+
+
+-- require('onedark').setup {
+--     style = 'deep'
+-- }
+-- require('onedark').load()
