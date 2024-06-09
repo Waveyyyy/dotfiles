22618e4cbef215fed8adb8faf5d151f201153cb6
Waveyyyy <waves-sec@protonmail.com>
2024-06-09 01:25:07 +0100

    Allow commenting of empty lines with nerdcommenter

--- desktop/neovim/.config/nvim/after/plugin/nerdcommenter.lua
+++ desktop/neovim/.config/nvim/after/plugin/nerdcommenter.lua
@@ -7,8 +7,14 @@ vim.g.NERDCompactSexyComs = 1
 -- Align line-wise comment delimiters flush left instead of following code indentation
 vim.g.NERDDefaultAlign = 'start'
 
--- Check all selected lines is commented or not 
+-- Check all selected lines is commented or not
 vim.g.NERDToggleCheckAllLines = 1
 
 -- Disable comment nesting (its just fucking annoying)
 vim.g.NERDDefaultNesting = 0
+
+-- Enable trimming of whitespace when uncommenting
+vim.g.NERDTrimTrailingWhitespace = 1
+
+-- Allow commenting and inverting empty lines
+vim.g.NERDCommentEmptyLines = 1
