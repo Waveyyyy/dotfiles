9ef3389780bf35eb5856844edd988622a2074c23
Waveyyyy <waves-sec@protonmail.com>
2024-06-09 01:00:22 +0100

    Update nvim-indentation configuration to work correctly with lua

--- desktop/neovim/.config/nvim/after/plugin/indentation.lua
+++ desktop/neovim/.config/nvim/after/plugin/indentation.lua
@@ -1,11 +1,36 @@
-local status, indent = pcall(require, "indent_blankline")
-if (not status) then return end
-
-vim.opt.list = false
-vim.opt.listchars:append "eol: "
-
-indent.setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = false,
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
 }
+
+local hooks = require "ibl.hooks"
+-- Create highlight groups in the highlight setup hook, so they are reset
+-- every time the colourscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07b" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+require("ibl").setup { indent = { highlight = highlight } }
+
+-- local status, indent = pcall(require, "indent_blankline")
+-- if (not status) then return end
+--
+-- vim.opt.list = false
+-- vim.opt.listchars:append "eol: "
+--
+-- indent.setup {
+--     space_char_blankline = " ",
+--     show_current_context = true,
+--     show_current_context_start = false,
+-- }
