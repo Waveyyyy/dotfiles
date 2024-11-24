e699dd32739613fa42377b586329abe1769d1e6f
Waveyyyy <waves-sec@protonmail.com>
2024-11-24 15:21:51 +0000

    Update packer bootstrap code

--- desktop/neovim/.config/nvim/lua/plugins.lua
+++ desktop/neovim/.config/nvim/lua/plugins.lua
@@ -1,19 +1,24 @@
-local fn = vim.fn
-
 -- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system({
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    })
-    print("Installing packer close and reopen Neovim...")
-    vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({
+            "git",
+            "clone",
+            "--depth",
+            "1",
+            "https://github.com/wbthomason/packer.nvim",
+            install_path,
+        })
+        print("Installing packer close and reopen Neovim...")
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
@@ -39,8 +44,7 @@ packer.init({
     },
 })
 
-packer.startup(function(use)
-    -- packer can manage itself!
+return packer.startup(function(use)
     use "wbthomason/packer.nvim"
 
     -- ranger file explorer
@@ -119,7 +123,7 @@ packer.startup(function(use)
 
     -- Automatically run packer.clean() followed by packer.update() after cloning packer.nvim
     -- Put this at the end after all plugins
-    if PACKER_BOOTSTRAP then
+    if packer_bootstrap then
         require("packer").sync()
     end
 end)
